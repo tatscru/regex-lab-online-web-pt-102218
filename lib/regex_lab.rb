436c4e40733bdeb29f59a005eece17082e76d4e1
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-06 00:41:54 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,7 @@
 def starts_with_a_vowel?(word)
   word.match(/^[aeiouAEIOU]/)? true : false  
   # remember this is another way to write an if/else statement. what happens on the left of the statement it would be true otherwise false
+  # the word can start with a vowel- lowercase or uppercase so you have to be sure you acknowledge both 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
