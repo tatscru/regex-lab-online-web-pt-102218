2775b5d0b96a7c9977443ed3dcec72ccfb89415b
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-06 01:28:33 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,7 +13,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z]+\.$/) == nil 
+  if text.match(/^[A-Z]. *\.$/) == nil 
       return false 
     else 
       return true
