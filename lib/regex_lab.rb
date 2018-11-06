2cf49d095ac0a66a277ad7fc6be7ab8b5d8ee2f8
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-06 01:20:51 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,7 +13,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/\w[a-z][A-Z]+.*$/)? false : true
+  text.scan(/\w[a-z][A-Z]+.*\.$/)? false : true
+  .*\.$/
 end
 
 def valid_phone_number?(phone)
