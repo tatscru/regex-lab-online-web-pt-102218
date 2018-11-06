4d82a4ded7336b2662745a0e2c0b6a2258a499c8
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-06 00:52:57 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ end
 def words_starting_with_un_and_ending_with_ing(text)
   text.scan(/\A+un\z+ing/)
   
-  text.scan(/\un\w+ing\b/)
+  text.scan(/\un\w+ing/)
 end 
 
 def words_five_letters_long(text)
