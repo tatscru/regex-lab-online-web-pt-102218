6c10b32ee76137f7b812eda0066313ebc0fa7e29
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-06 01:32:18 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,27 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/^[aeiouAEIOU]/)? true : false  
+  # remember this is another way to write an if/else statement. what happens on the left of the statement it would be true otherwise false
+  # the word can start with a vowel- lowercase or uppercase so you have to be sure you acknowledge both 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
-end
+  text.scan(/un\w+ing\b/)
+end 
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  if text.match(/^[A-Z].*\.$/) == nil 
+      return false 
+    else 
+      return true
+  end
+end 
 
-end
 
 def valid_phone_number?(phone)
+  phone.match(/\d [0-9]/)
 
 end
