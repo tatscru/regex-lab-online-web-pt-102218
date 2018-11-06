22e16360f381e44b6f1e1252f74823d580033d04
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-06 01:18:22 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,7 +13,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/\w[A-Z]+.$/)? true : false
+  text.scan(/\w[a-z][A-Z]+.$/)? false : true
 end
 
 def valid_phone_number?(phone)
