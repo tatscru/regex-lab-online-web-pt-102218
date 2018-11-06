63f01a48a0ddf25dd51955ffe26afbc12690f48d
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-06 01:29:04 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,19 +20,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
   end
 end 
 
-#   if text.match(/^[A-Z].*\.$/) == nil
-#     return false
-#   else
-#     return true
-#   end
-# end
-
-#   if text.scan(/^[A-Z][\s\S]+\.$/).empty?
-#     false
-#   else
-#     true
-#   end
-# end
 
 def valid_phone_number?(phone)
 
